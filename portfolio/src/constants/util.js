16bcb73fdec26ab804b1da9b01db4161f93f4bc3
Ajay-S-Biradar <sbmunnu@gmail.com>
2024-08-01 00:45:21 +0530

    added the backend link

--- portfolio/src/constants/util.js
+++ portfolio/src/constants/util.js
@@ -1,1 +1,2 @@
-export const API_URL = "http://localhost:2004/api/assistant/";
+export const API_URL = "portfolio-editor-git-main-ajaysbiradars-projects.vercel.app/"
+//"http://localhost:2004/api/assistant/";
